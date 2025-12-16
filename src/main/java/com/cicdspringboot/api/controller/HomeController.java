fdf6f341304499c2da9840dfafb357deb159ec3a
bhaskar4k <bhaskarndy@gmail.com>
2025-12-16 11:26:07 +0530

    Project first push

---
+++ src/main/java/com/cicdspringboot/api/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.cicdspringboot.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("Home")
+public class HomeController {
+    @GetMapping("HelloCiCd")
+    public String HelloCiCd(){
+        return "CI/CD is working";
+    }
+}
