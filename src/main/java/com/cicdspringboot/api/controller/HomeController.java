70c49dfebabf0fd91dd7d7d4dae4e635a915c750
bhaskar4k <bhaskarndy@gmail.com>
2025-12-16 13:10:45 +0530

    Updated controller

--- src/main/java/com/cicdspringboot/api/controller/HomeController.java
+++ src/main/java/com/cicdspringboot/api/controller/HomeController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("HelloCiCd")
     public String HelloCiCd(){
-        return "CI/CD is working.";
+        return "CI/CD is working on localhost.";
     }
 }
